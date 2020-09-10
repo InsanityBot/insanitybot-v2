f890105c9611c3337c26d18b548e94c040b34830
ExaInsanity <61018569+ExaInsanity@users.noreply.github.com>
2020-09-10 18:08:54 +0200

    Permissions base setup

---
+++ InsanityBot/InsanityBot.Utility/Permissions/DiscordClientBinding.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+using DSharpPlus;
+using DSharpPlus.EventArgs;
+
+using InsanityBot.Utility.Permissions.Serialization;
+
+using Newtonsoft.Json;
+
+#pragma warning disable CS1998 // these async methods dont technically need to run async, its just the API requiring them to be marked as async
+
+namespace InsanityBot.Utility.Permissions
+{
+    public static class DiscordClientBinding
+    {
+        public static void InitializePermissionFramework(this DiscordClient client)
+        {
+            client.GuildMemberAdded += GuildMemberAdded;
+            client.GuildMemberRemoved += GuildMemberRemoved;
+            client.GuildMemberUpdated += GuildMemberUpdated;
+        }
+
+        private static async Task GuildMemberUpdated(GuildMemberUpdateEventArgs e)
+        {
+            if (e.RolesBefore == e.RolesAfter)
+                return;
+
+            if(e.Member.PermissionsIn(e.Guild.GetDefaultChannel()).HasPermission(DSharpPlus.Permissions.KickMembers))
+            {
+                UserPermission permission = e.Member.GetPermissions();
+                permission.Permissions.VerbalWarn = true;
+                permission.Permissions.Warn = true;
+                permission.Permissions.Unwarn = true;
+                permission.Permissions.Mute = true;
+                permission.Permissions.Tempmute = true;
+                permission.Permissions.Unmute = true;
+                permission.Permissions.Blacklist = true;
+                permission.Permissions.Whitelist = true;
+                permission.Permissions.Kick = true;
+                permission.Permissions.Slowmode = true;
+            }
+
+            if(e.Member.PermissionsIn(e.Guild.GetDefaultChannel()).HasPermission(DSharpPlus.Permissions.BanMembers))
+            {
+                UserPermission permission = e.Member.GetPermissions();
+                permission.Permissions.Ban = true;
+                permission.Permissions.Tempban = true;
+                permission.Permissions.Unban = true;
+                permission.Permissions.Lock = true;
+                permission.Permissions.Unlock = true;
+            }
+
+            if(e.Member.PermissionsIn(e.Guild.GetDefaultChannel()).HasPermission(DSharpPlus.Permissions.Administrator))
+            {
+                UserPermission permission = e.Member.GetPermissions();
+                permission.Permissions.SuggestionAccept = true;
+                permission.Permissions.SuggestionDeny = true;
+                permission.Permissions.TicketApplyAccept = true;
+                permission.Permissions.TicketApplyDeny = true;
+
+                permission.Permissions.Archive = true;
+                permission.Permissions.Config = true;
+                permission.Permissions.Permission = true;
+            }
+        }
+
+        private static async Task GuildMemberRemoved(GuildMemberRemoveEventArgs e)
+        {
+            Directory.Delete($"./data/{e.Member.Id}", true);
+        }
+
+        private static async Task GuildMemberAdded(GuildMemberAddEventArgs e)
+        {
+            Directory.CreateDirectory($"./data/{e.Member.Id}");
+            File.Create($"./data/{e.Member.Id}/permissions.json");
+
+            FileStream file = new FileStream($"./data/{e.Member.Id}/permissions.json", FileMode.Truncate);
+            StreamWriter writer = new StreamWriter(file);
+
+            Serialization.Reference.Permissions permissions = new Serialization.Reference.Permissions();
+
+            await writer.WriteAsync(JsonConvert.SerializeObject(permissions));
+        }
+    }
+}
---
+++ InsanityBot/InsanityBot.Utility/Permissions/PermissionCache.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Timers;
+
+using DSharpPlus.Entities;
+
+using InsanityBot.Utility.Permissions.Serialization;
+
+using Newtonsoft.Json;
+
+namespace InsanityBot.Utility.Permissions
+{
+    public static class PermissionCache
+    {
+        private static List<UserPermission> UserPermissions { get; set; }
+
+        private static Timer CacheHeartbeat { get; set; }
+
+        private static void CacheHeartbeat_Elapsed(Object sender, ElapsedEventArgs e)
+        {
+            foreach(var p in UserPermissions)
+            {
+                if (p.HeartbeatsUnused == 5)
+                    UserPermissions.Remove(p);
+                else
+                    p.HeartbeatsUnused++;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the Timer and the Cache
+        /// </summary>
+        public static void Initialize()
+        {
+            CacheHeartbeat = new Timer
+            {
+                AutoReset = true,
+                Interval = 30000
+            };
+            CacheHeartbeat.Elapsed += CacheHeartbeat_Elapsed;
+
+            UserPermissions = new List<UserPermission>();
+        }
+
+        private static UserPermission LoadNew(UInt64 Id)
+        {
+            StreamReader reader = new StreamReader($"./data/{Id}/permissions.json");
+            UserPermission permissions = (UserPermission)JsonConvert.DeserializeObject(reader.ReadToEnd());
+
+            permissions.UserID = Id;
+            permissions.HeartbeatsUnused = 0;
+
+            UserPermissions.Add(permissions);
+            return permissions;
+        }
+
+        // not happy with this one, someone please improve it
+
+        /// <summary>
+        /// Fetches the UserPermission object for a certain member
+        /// </summary>
+        public static UserPermission GetUserPermission(UInt64 Id)
+        {
+            var v = from vx in UserPermissions
+                    where vx.UserID == Id
+                    select vx;
+
+            UserPermission returnValue = (v.Count()) switch
+            {
+                0 => LoadNew(Id),
+                1 => v.First(),
+                _ => throw new InvalidOperationException("InsanityBot.Utility.Permissions.PermissionCache.cs: Duplicate cache entries")
+            };
+
+            UserPermissions.Remove(returnValue);
+            returnValue.HeartbeatsUnused = 0;
+            UserPermissions.Add(returnValue);
+
+            return returnValue;
+        }
+
+        private static void Serialize(UserPermission permissions)
+        {
+            FileStream file = new FileStream($"./data/{permissions.UserID}/permissions.json", FileMode.Truncate);
+            StreamWriter writer = new StreamWriter(file);
+
+            writer.Write(JsonConvert.SerializeObject(permissions));
+        }
+
+        /// <summary>
+        /// Fetches the UserPermission object for this member.
+        /// </summary>
+        public static UserPermission GetPermissions(this DiscordMember member)
+        {
+            var v = from vx in UserPermissions
+                    where vx.UserID == member.Id
+                    select vx;
+
+            UserPermission returnValue = (v.Count()) switch
+            {
+                0 => LoadNew(member.Id),
+                1 => v.First(),
+                _ => throw new InvalidOperationException("InsanityBot.Utility.Permissions.PermissionCache.cs: Duplicate cache entries")
+            };
+
+            UserPermissions.Remove(returnValue);
+            returnValue.HeartbeatsUnused = 0;
+            UserPermissions.Add(returnValue);
+
+            return returnValue;
+        }
+
+        public static void SetPermissions(this DiscordMember member, UserPermission permissions)
+        {
+            var v = from vx in UserPermissions
+                    where vx.UserID == member.Id
+                    select vx;
+
+            switch (v.Count())
+            {
+                case 0:
+                    Serialize(permissions);
+                    return;
+                case 1:
+                    UserPermissions.Remove(v.First());
+                    return;
+                default:
+                    throw new InvalidOperationException("InsanityBot.Utility.Permissions.PermissionCache.cs: Duplicate cache entries");
+            }
+        }
+    }
+}
