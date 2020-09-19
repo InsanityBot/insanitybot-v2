b274f2a0f9357c2a20e4b6d452ea805b83a833e6
ExaInsanity <61018569+ExaInsanity@users.noreply.github.com>
2020-09-19 09:33:27 +0200

    InsanityBot.Main method

--- InsanityBot/InsanityBot/InsanityBot.cs
+++ InsanityBot/InsanityBot/InsanityBot.cs
@@ -3,15 +3,23 @@ using System.Collections.Generic;
 using System.Text;
 
 using DSharpPlus;
+using DSharpPlus.CommandsNext;
 using DSharpPlus.Entities;
+
+using InsanityBot.Utility.Config;
 
 namespace InsanityBot
 {
     public partial class InsanityBot
     {
+        public static MainConfig Config;
+
         public static DiscordConfiguration ClientConfiguration;
         public static DiscordClient Client;
 
-        public static DiscordGuild HomeGuild;    
+        public static DiscordGuild HomeGuild;
+
+        public static CommandsNextExtension CommandsExtension;
+        public static CommandsNextConfiguration CommandConfiguration;
     }
 }
--- InsanityBot/InsanityBot/Main.cs
+++ InsanityBot/InsanityBot/Main.cs
@@ -1,8 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading;
+using System.Linq;
 using System.Threading.Tasks;
+
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+
+using InsanityBot.Utility.Config;
+
+using Microsoft.Extensions.Logging;
 
 namespace InsanityBot
 {
@@ -15,15 +20,84 @@ namespace InsanityBot
             CommandLine.InsanityBotApplication.Execute(args);
 
             //reset if reset flag is set
-            if (InsanityBot.CommandLine.HardResetOnStartup.HasValue())
+            if (CommandLine.HardResetOnStartup.HasValue())
                 await HardReset();
 
             //initialize if init flag is set
-            if (InsanityBot.CommandLine.InitializeOnStartup.HasValue())
+            if (CommandLine.InitializeOnStartup.HasValue())
                 await Initialize();
 
-            //initialization finished, abort main thread
+            //load main config
+            Config = MainConfigManager.DeserializeMainConfiguration();
+
+            //create discord config; increase the cache size if you want though itll take more RAM
+            ClientConfiguration = new DiscordConfiguration
+            {
+                AutoReconnect = true,
+                Token = Config.Token,
+                TokenType = TokenType.Bot,
+                MessageCacheSize = 2048,
+#if DEBUG
+                MinimumLogLevel = LogLevel.Debug
+#else
+                MinimumLogLevel = LogLevel.Information
+#endif
+            };
+
+            //create and connect client
+            Client = new DiscordClient(ClientConfiguration);
+            await Client.ConnectAsync();
+
+            //set home guild to speed up bot performance later
+            HomeGuild = await Client.GetGuildAsync(Config.DiscordConfig.Identifiers.GuildId);
+
+            //initialize config
+            CommandConfiguration = new CommandsNextConfiguration
+            {
+                CaseSensitive = false,
+                StringPrefixes = Config.MainPrefix
+                    .Concat(Config.AdminPrefix)
+                    .ToList()
+            };
+            Client.UseCommandsNext(CommandConfiguration);
+
+            //get command framework extension
+            CommandsExtension = Client.GetCommandsNext();
+
+            //register all top-level command classes
+            RegisterAllCommands();
+
+            //register all client events to their respective methods
+            RegisterAllEvents();
+
+            //change help command formatting
+            FormatHelpCommand();
+
+            //handle TCP Connections for services like HetrixTools
+            HandleTCPConnections();
+
+            //initialization finished, abort main thread, who needs it anyway
             await Task.Delay(-1);
+        }
+
+        private static void RegisterAllCommands()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void RegisterAllEvents()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void FormatHelpCommand()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void HandleTCPConnections()
+        {
+            throw new NotImplementedException();
         }
     }
 }
