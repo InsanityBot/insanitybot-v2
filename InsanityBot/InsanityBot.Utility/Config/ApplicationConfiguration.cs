0dcdcf0fa683c2203823de98c56b935381a44c74
ExaInsanity <61018569+ExaInsanity@users.noreply.github.com>
2020-09-24 10:02:25 +0200

    Update ApplicationConfiguration

---
+++ InsanityBot/InsanityBot.Utility/Config/ApplicationConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InsanityBot.Utility.Config
+{
+    public class ApplicationConfiguration : IConfiguration
+    {
+        public String DataVersion { get; set; }
+        public Dictionary<String, Object> Configuration { get; set; }
+        public String Identifier { get; set; }
+    }
+}
