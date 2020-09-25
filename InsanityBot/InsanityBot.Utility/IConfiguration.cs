ad4d131a12f332d8b007d9ac186f6d9b12bb07ef
ExaInsanity <61018569+ExaInsanity@users.noreply.github.com>
2020-09-25 15:49:24 +0200

    Explain type safety issue

--- InsanityBot/InsanityBot.Utility/IConfiguration.cs
+++ InsanityBot/InsanityBot.Utility/IConfiguration.cs
@@ -7,6 +7,8 @@ namespace InsanityBot.Utility
     public interface IConfiguration
     {
         public String DataVersion { get; set; }
+
+        // if anyone knows how to make this fully typesafe while keeping compatibility for String, Boolean, all those ints and IEnumerable, submit a PR
         public Dictionary<String, Object> Configuration { get; set; }
     }
 }
