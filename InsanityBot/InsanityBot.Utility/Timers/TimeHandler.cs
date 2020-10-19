bc22e3aec675b6af68935eb168794c4b39bb0c72
ExaInsanity <61018569+ExaInsanity@users.noreply.github.com>
2020-10-19 11:56:18 +0200

    Add a static handler for the timers

---
+++ InsanityBot/InsanityBot.Utility/Timers/TimeHandler.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Text;
+
+using Newtonsoft.Json;
+
+namespace InsanityBot.Utility.Timers
+{
+    public static class TimeHandler
+    {
+        public static void Start()
+        {
+            if(!Directory.Exists("./data/timers"))
+            {
+                Directory.CreateDirectory("./data/timers");
+                return;
+            }
+
+            //knowing that it exists, proceed to read contents
+
+            if (Directory.GetFiles("./data/timers").Length == 0)
+                return;
+
+            //ok, it exists and has file contents. time to read.
+
+            StreamReader reader;
+
+            foreach (String s in Directory.GetFiles("./data/timers"))
+            {
+                reader = new StreamReader(File.OpenRead(s));
+                ActiveTimers.Add(JsonConvert.DeserializeObject<Timer>(reader.ReadToEnd()));
+            }
+        }
+
+        public static void Stop()
+        {
+            if (!Directory.Exists("./data/timers"))
+                Directory.CreateDirectory("./data/timers");
+
+            if (ActiveTimers.Count == 0)
+                return;
+
+            StreamWriter writer;
+
+            foreach(Timer t in ActiveTimers)
+            {
+                if (File.Exists($"./data/timers/{t.Identifier}"))
+                    File.Create($"./data/timers/{t.Identifier}").Close();
+                writer = new StreamWriter(File.Open($"./data/timers/{t.Identifier}", FileMode.Truncate));
+
+                writer.Write(JsonConvert.SerializeObject(t));
+            }
+        }
+
+        public static List<Timer> ActiveTimers { get; set; }
+    }
+}
