517b1713e6ee36b5693a4cf2f191af421b64d9ac
ExaInsanity <61018569+ExaInsanity@users.noreply.github.com>
2020-10-19 15:18:45 +0200

    Add automatic updates to the timer

--- InsanityBot/InsanityBot.Utility/Timers/TimeHandler.cs
+++ InsanityBot/InsanityBot.Utility/Timers/TimeHandler.cs
@@ -12,7 +12,7 @@ namespace InsanityBot.Utility.Timers
     {
         public static void Start()
         {
-            if(!Directory.Exists("./data/timers"))
+            if (!Directory.Exists("./data/timers"))
             {
                 Directory.CreateDirectory("./data/timers");
                 return;
@@ -32,6 +32,18 @@ namespace InsanityBot.Utility.Timers
                 reader = new StreamReader(File.OpenRead(s));
                 ActiveTimers.Add(JsonConvert.DeserializeObject<Timer>(reader.ReadToEnd()));
             }
+
+            foreach (Timer t in ActiveTimers)
+                //disable the warning, why tf would it exist; this isnt an async method
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+                t.CheckExpiry();
+
+            Countdown = new System.Timers.Timer
+            {
+                AutoReset = true,
+                Interval = 500
+            };
+            Countdown.Elapsed += CountdownElapsed;
         }
 
         public static void Stop()
@@ -44,7 +56,7 @@ namespace InsanityBot.Utility.Timers
 
             StreamWriter writer;
 
-            foreach(Timer t in ActiveTimers)
+            foreach (Timer t in ActiveTimers)
             {
                 if (File.Exists($"./data/timers/{t.Identifier}"))
                     File.Create($"./data/timers/{t.Identifier}").Close();
@@ -54,6 +66,15 @@ namespace InsanityBot.Utility.Timers
             }
         }
 
+        private static void CountdownElapsed(Object sender, System.Timers.ElapsedEventArgs e)
+        {
+            foreach (Timer t in ActiveTimers)
+                t.CheckExpiry();
+            //and here we restore the warning again
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+        }
+
         public static List<Timer> ActiveTimers { get; set; }
+        private static System.Timers.Timer Countdown { get; set; }
     }
 }
