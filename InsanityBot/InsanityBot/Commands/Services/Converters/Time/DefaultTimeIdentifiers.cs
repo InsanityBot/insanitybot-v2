668ef31efb05d7d434864fea1315887ad3af0cf8
ExaInsanity <61018569+ExaInsanity@users.noreply.github.com>
2020-12-13 13:38:03 +0100

    add things you should never be using directly

---
+++ InsanityBot/InsanityBot/Commands/Services/Converters/Time/DefaultTimeIdentifiers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsanityBot.Commands.Services.Converters.Time
+{
+    internal struct DefaultTimeIdentifiers
+    {
+        internal static TimeIdentifier Second = new TimeIdentifier(1000, 's', "second", "sec", "seconds");
+        internal static TimeIdentifier Minute = new TimeIdentifier(60000, 'm', "minute", "min", "minutes");
+        internal static TimeIdentifier Hour = new TimeIdentifier(3600000, 'h', "hour", "hr", "hrs", "hours");
+        internal static TimeIdentifier Day = new TimeIdentifier(86400000, 'd', "day", "d", "days");
+        internal static TimeIdentifier Week = new TimeIdentifier(604800000, 'w', "week", "weeks");
+        internal static TimeIdentifier Month = new TimeIdentifier(2630016000, 'o', "month", "months", "mo");
+        internal static TimeIdentifier Year = new TimeIdentifier(31536000000, 'y', "year", "years", "yr", "yrs");
+    }
+}
---
+++ InsanityBot/InsanityBot/Commands/Services/Converters/Time/TimeIdentifier.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsanityBot.Commands.Services.Converters.Time
+{
+    internal sealed class TimeIdentifier
+    {
+        internal UInt64 Ticks { get; init; }
+        internal Char PrimaryIdentifier { get; init; }
+        internal String[] SecondaryIdentifiers { get; init; }
+        internal String FullName { get; init; }
+
+        internal TimeIdentifier(UInt64 ticks, Char identifier, String name, params String[] secondaries) =>
+            (Ticks, PrimaryIdentifier, SecondaryIdentifiers, FullName) = (ticks, identifier, secondaries, name);
+    }
+}
