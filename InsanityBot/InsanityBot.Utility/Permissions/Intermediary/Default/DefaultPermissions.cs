38ba3cdbc60f415dca7b6104764bf3da9020d650
ExaInsanity <61018569+ExaInsanity@users.noreply.github.com>
2021-03-09 11:02:53 +0100

    Use SHA512 checksums to distinguish permission files

--- InsanityBot/InsanityBot.Utility/Permissions/Intermediary/Default/DefaultPermissions.cs
+++ InsanityBot/InsanityBot.Utility/Permissions/Intermediary/Default/DefaultPermissions.cs
@@ -1,10 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Reflection.Metadata.Ecma335;
-using System.Text;
-using System.Threading.Tasks;
 
 using InsanityBot.Utility.Permissions.Reference;
 
---
+++ InsanityBot/InsanityBot.Utility/SHA512HashMap.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+
+namespace InsanityBot.Utility
+{
+    public class SHA512HashMap<TValue> : Dictionary<String, TValue>
+    {
+        protected TValue NullValue { get; set; } = default;
+
+        public new TValue this[String key]
+        {
+            get
+            {
+                if (!ContainsKey(key))
+                    return NullValue;
+                return base[key];
+            }
+            set
+            {
+                if (ContainsKey(key))
+                    base[key] = value;
+                else
+                    Add(key, value);
+            }
+        }
+
+        public void Add(TValue value)
+        {
+            SHA512 sha512Provider = SHA512.Create();
+            Add(Encoding.UTF8.GetString(sha512Provider.ComputeHash(Encoding.UTF8.GetBytes(value.ToString()))), value);
+        }
+    }
+}
