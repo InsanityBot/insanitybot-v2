da5e26a58af080535eb859cee0102ad0f6308a10
ExaInsanity <61018569+ExaInsanity@users.noreply.github.com>
2021-03-10 12:27:37 +0100

    Add managing for DefaultPermissions

---
+++ InsanityBot/InsanityBot.Utility/Permissions/Controller/DefaultPermissionUpdater.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using InsanityBot.Utility.Permissions.Model;
+
+using Newtonsoft.Json;
+
+namespace InsanityBot.Utility.Permissions.Controller
+{
+    public static class DefaultPermissionUpdater
+    {
+        public const String IntermediateFilePath = "./cache/permissions/intermediate";
+        public const String ModPermissionFilePath = "./mod-data/permissions";
+
+        public static Boolean CheckForUpdates()
+        {
+            StreamReader reader = new($"{IntermediateFilePath}/default-checksums");
+            SHA512HashMap<String> fileChecksums = JsonConvert.DeserializeObject<SHA512HashMap<String>>(reader.ReadToEnd());
+
+            if (!(fileChecksums["./config/permissions/vanilla.pdecl.json".GetSHA512Checksum()] == "vanilla"))
+                return true;
+
+            var modFiles = (from x in Directory.GetFiles($"{ModPermissionFilePath}")
+                     where x.EndsWith(".pdecl.json")
+                     select x).ToList();
+
+            foreach(var v in modFiles)
+            {
+                if (!fileChecksums.Keys.Contains(v.GetSHA512Checksum()))
+                    return true;
+            }
+
+            return false;
+        }
+
+        public static DefaultPermissions BuildDefaultPermissions()
+        {
+            DefaultPermissions permissions = DefaultPermissions.Empty;
+
+            StreamReader reader = new($"./config/permissions/vanilla.pdecl.json");
+            permissions += JsonConvert.DeserializeObject<PermissionDeclaration[]>(reader.ReadToEnd());
+            reader.Close();
+
+            var modFiles = (from x in Directory.GetFiles($"{ModPermissionFilePath}")
+                            where x.EndsWith(".pdecl.json")
+                            select x).ToList();
+
+            foreach(var v in modFiles)
+            {
+                reader = new(v);
+                permissions += JsonConvert.DeserializeObject<PermissionDeclaration[]>(reader.ReadToEnd());
+                reader.Close();
+            }
+
+            StreamWriter writer = new($"./config/permissions/default.json");
+            writer.Write(JsonConvert.SerializeObject(permissions));
+            writer.Close();
+
+            UpdateChecksumData();
+
+            return permissions;
+        }
+
+        public static void UpdateChecksumData()
+        {
+            SHA512HashMap<String> map = new();
+
+            map.Add($"./config/permissions/vanilla.pdecl.json".GetSHA512Checksum(), "vanilla");
+
+            var modFiles = (from x in Directory.GetFiles($"{ModPermissionFilePath}")
+                            where x.EndsWith(".pdecl.json")
+                            select x).ToList();
+
+            foreach(var v in modFiles)
+            {
+                String modName = v.Split('\\')
+                    .Last()
+                    .Split('.')[0];
+
+                map.Add(v.GetSHA512Checksum(), modName);
+            }
+
+            StreamWriter writer = new(File.Open($"{IntermediateFilePath}/default-checksums", FileMode.Truncate));
+            writer.Write(JsonConvert.SerializeObject(map));
+            writer.Close();
+        }
+    }
+}
--- InsanityBot/InsanityBot.Utility/Permissions/Model/DefaultPermissions.cs
+++ InsanityBot/InsanityBot.Utility/Permissions/Model/DefaultPermissions.cs
@@ -13,5 +13,52 @@ namespace InsanityBot.Utility.Permissions.Model
             get => 0;
             set => base.SnowflakeIdentifier = 0;
         }
+
+        public static DefaultPermissions Empty
+        {
+            get => new();
+        }
+
+        public DefaultPermissions()
+        {
+            SnowflakeIdentifier = 0;
+            IsAdministrator = false;
+            Permissions = new();
+        }
+
+        public static DefaultPermissions operator +(DefaultPermissions left, DefaultPermissions right)
+        {
+            DefaultPermissions permissions = Empty;
+
+            if (left.IsAdministrator || right.IsAdministrator)
+                permissions.IsAdministrator = true;
+
+            foreach (var v in left.Permissions)
+                permissions.Permissions[v.Key] = v.Value;
+
+            foreach (var v in right.Permissions)
+                permissions.Permissions[v.Key] = v.Value;
+
+            return permissions;
+        }
+
+        public static DefaultPermissions operator +(DefaultPermissions left, PermissionDeclaration right)
+        {
+            DefaultPermissions permissions = left;
+
+            permissions.Permissions[right.Permission] = right.Default;
+
+            return permissions;
+        }
+
+        public static DefaultPermissions operator +(DefaultPermissions left, PermissionDeclaration[] right)
+        {
+            DefaultPermissions permissions = left;
+
+            foreach (var v in right)
+                left.Permissions[v.Permission] = v.Default;
+
+            return permissions;
+        }
     }
 }
