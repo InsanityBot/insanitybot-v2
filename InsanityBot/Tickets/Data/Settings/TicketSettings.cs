46d2ae4a9f903f3be9315ac9de5ffeb875c5fc96
ExaInsanity <61018569+ExaInsanity@users.noreply.github.com>
2021-04-11 04:59:21 +0200

    Add ticket settings

---
+++ InsanityBot/Tickets/Data/Settings/TicketSettings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsanityBot.Tickets.Data.Settings
+{
+    public record TicketSettings
+    {
+        public Boolean MetricFirstResponse { get; set; }
+        public Boolean MetricResponseTime { get; set; }
+        public Boolean MetricResolutionTime { get; set; }
+        public Boolean MetricMessageCount { get; set; }
+        public Boolean MetricMessagePerResolution { get; set; }
+
+        public Boolean MentionStaffOnResponse { get; set; }
+        public Boolean MentionClientOnResponse { get; set; }
+        public Boolean MentionStaffRoleOnResponse { get; set; }
+        public Boolean MentionAssignedStaffOnResponse { get; set; }
+        public Boolean MentionAllClientsOnResponse { get; set; }
+
+        public Boolean AutoAssignStaff { get; set; }
+        public Boolean AllowAddUsers { get; set; }
+    }
+}
