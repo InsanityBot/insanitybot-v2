3b25aeaff70c9b906eb3a89c5e41e0043ba153da
ExaInsanity <61018569+ExaInsanity@users.noreply.github.com>
2021-04-18 18:52:05 +0200

    Add Ticket Presets

---
+++ InsanityBot/Tickets/TicketPreset.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using DSharpPlus.Entities;
+
+namespace InsanityBot.Tickets
+{
+    public record TicketPreset
+    {
+        public UInt64 Category { get; set; }
+        public String NameFormat { get; set; }
+        public String Topic { get; set; }
+        public TicketAccess AccessRules { get; set; }
+        public DiscordMessage[] CreationMessages { get; set; }
+        public DiscordEmbed[] CreationEmbeds { get; set; }
+    }
+}
