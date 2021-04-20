cdf535b52d176cf16a28250260d3727e8a051691
ExaInsanity <61018569+ExaInsanity@users.noreply.github.com>
2021-04-20 19:29:58 +0200

    Add a separate ticket configuration

---
+++ InsanityBot/Tickets/Daemon/Config/TicketConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using InsanityBot.Utility;
+using InsanityBot.Utility.Reference;
+
+namespace InsanityBot.Tickets.Daemon.Config
+{
+    class TicketConfiguration : IConfiguration<Object>
+    {
+        public String DataVersion { get; set; }
+        public Dictionary<String, Object> Configuration { get; set; }
+
+        public Object this[String Identifier]
+        {
+            get => Configuration[Identifier];
+            set => Configuration[Identifier] = value;
+        }
+
+        public TicketConfiguration()
+        {
+            DataVersion = "2.0.0";
+            Configuration = new();
+        }
+    }
+}
---
+++ InsanityBot/Tickets/Daemon/Config/TicketConfigurationManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using InsanityBot.Utility;
+using InsanityBot.Utility.Config;
+using InsanityBot.Utility.Datafixers;
+
+using Newtonsoft.Json;
+
+namespace InsanityBot.Tickets.Daemon.Config
+{
+    class TicketConfigurationManager : IConfigSerializer<TicketConfiguration, Object>,
+        IConfigBuilder<TicketConfiguration, TicketConfigurationManager, Object>
+    {
+        public TicketConfiguration Config { get; set; }
+
+        public TicketConfigurationManager AddConfigEntry(String Identifier, Object DefaultValue)
+        {
+            Config.Configuration.Add(Identifier, DefaultValue);
+            return this;
+        }
+
+        public TicketConfigurationManager RemoveConfigEntry(String Identifier)
+        {
+            Config.Configuration.Remove(Identifier);
+            return this;
+        }
+
+        public TicketConfiguration Deserialize(String Filename)
+        {
+            using StreamReader reader = new(File.OpenRead(Filename));
+
+            TicketConfiguration config = JsonConvert.DeserializeObject<TicketConfiguration>(reader.ReadToEnd());
+            config = (TicketConfiguration)DataFixerLower.UpgradeData(config);
+            reader.Close();
+
+            Serialize(config, "./config/ticket.json");
+            return config;
+        }
+
+        public void Serialize(TicketConfiguration Config, String Filename)
+        {
+            using StreamWriter writer = new(File.OpenWrite(Filename));
+            writer.BaseStream.SetLength(0);
+            writer.Flush();
+            writer.Write(JsonConvert.SerializeObject(Config, Formatting.Indented));
+        }
+    }
+}
