5f41ed071c22073bce0c68bfea5e377f571a5b33
ExaInsanity <61018569+ExaInsanity@users.noreply.github.com>
2021-04-27 21:58:53 +0200

    Add DiscordEmbed kyuu datatype

---
+++ InsanityBot/Tickets/Kyuu/Runtime/Variables/Datatypes/MockDiscordEmbedField.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsanityBot.Tickets.Kyuu.Runtime.Variables.Datatypes
+{
+    public struct MockDiscordEmbedField
+    {
+        public Boolean Inline { get; set; }
+        public String Name { get; set; }
+        public String Value { get; set; }
+    }
+}
