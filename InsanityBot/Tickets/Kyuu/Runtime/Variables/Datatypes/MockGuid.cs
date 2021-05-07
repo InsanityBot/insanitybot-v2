4ee63db967c86dcccc786aea3dbbade3e36a57bb
ExaInsanity <61018569+ExaInsanity@users.noreply.github.com>
2021-05-07 10:52:01 +0200

    Add a mock GUID type
    This is the last type that cant be mirrored directly into dotnet framework types, and even this type only needs its own mock to convert without having to take the step via json

---
+++ InsanityBot/Tickets/Kyuu/Runtime/Variables/Datatypes/MockGuid.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsanityBot.Tickets.Kyuu.Runtime.Variables.Datatypes
+{
+    public struct MockGuid
+    {
+        public String Value { get; set; }
+
+        public static explicit operator Guid(MockGuid guid)
+        {
+            return new(Encoding.UTF8.GetBytes(guid.Value));
+        }
+
+        public static explicit operator MockGuid(Guid guid)
+        {
+            return new()
+            {
+                Value = guid.ToString()
+            };
+        }
+
+        public MockGuid(String @string)
+        {
+            this.Value = @string;
+        }
+    }
+}
