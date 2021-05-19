f66eaf03bcf631f6975a9decda71021646a28aaa
Iris Féanorá <61018569+ExaInsanity@users.noreply.github.com>
2021-05-19 16:32:10 +0200

    Update dependencies and bump version

--- InsanityBot/InsanityBot.cs
+++ InsanityBot/InsanityBot.cs
@@ -38,7 +38,7 @@ namespace InsanityBot
 
         public static PermissionEngine PermissionEngine;
 
-        public static String Version = "2.0.0-dev.00030";
+        public static String Version = "2.0.0-dev.00031";
     }
 }
 #pragma warning restore CA2211
