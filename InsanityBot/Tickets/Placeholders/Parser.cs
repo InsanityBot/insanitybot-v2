eb2aa70ff286108254cfe22ec339908bd4da307c
ExaInsanity <nicolaiandorfer@outlook.com>
2021-06-20 20:45:08 +0200

    parse length-specific parameters

--- InsanityBot/Tickets/Placeholders/Parser.cs
+++ InsanityBot/Tickets/Placeholders/Parser.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace InsanityBot.Tickets.Placeholders
@@ -14,9 +15,38 @@ namespace InsanityBot.Tickets.Placeholders
 				return Task.FromResult(tempValue);
 			}
 
-			foreach (System.Collections.Generic.KeyValuePair<String, Func<DiscordTicket, String>> v in PlaceholderList.Placeholders)
+			foreach (KeyValuePair<String, Func<DiscordTicket, String>> v in PlaceholderList.Placeholders)
 			{
 				tempValue = tempValue.Replace($"{{{v.Key}}}", v.Value(InsanityBot.TicketDaemon.Tickets[ticket]));
+
+				if(!tempValue.Contains($"{{{v.Key}:"))
+				{
+					continue;
+				}
+
+				// unoptimized. feel free to improve
+				List<Int32> indices = new();
+				List<Byte> lengthValues = new();
+
+				for(Int32 i = 0; i < tempValue.Length; i++)
+				{
+					if (tempValue[i] != '{')
+						continue;
+
+					if (tempValue[i..(i + v.Key.Length + 1)] != $"{{{v.Key}:")
+						continue;
+
+					indices.Add(i);
+
+					Int32 closingBracketIndex = tempValue.Substring((i + v.Key.Length + 1), 3).IndexOf('}');
+					lengthValues.Add(Convert.ToByte(tempValue[(i + v.Key.Length + 1)..(closingBracketIndex + 1)]));
+				}
+
+				for(Byte b = 0; b < indices.Count; b++)
+				{
+					tempValue = tempValue.Replace($"{{{v.Key}:{lengthValues[b]}}}",
+						$"{v.Value(InsanityBot.TicketDaemon.Tickets[ticket]).Substring(0, lengthValues[b])}");
+				}
 			}
 
 			return Task.FromResult(tempValue);
