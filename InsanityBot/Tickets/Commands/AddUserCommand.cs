9786ffc4f69a3745118c6a20458cf8deb1427009
ExaInsanity <nicolaiandorfer@outlook.com>
2021-08-01 15:01:52 +0200

    add roles to tickets

--- InsanityBot/Tickets/Commands/AddUserCommand.cs
+++ InsanityBot/Tickets/Commands/AddUserCommand.cs
@@ -44,5 +44,37 @@ namespace InsanityBot.Tickets.Commands
                 }
             }
         }
+
+        public async Task AddRole(CommandContext ctx,
+            params DiscordRole[] roles)
+        {
+            IEnumerable<KeyValuePair<Guid, DiscordTicket>> x = from t in InsanityBot.TicketDaemon.Tickets
+                                                               where t.Value.DiscordChannelId == ctx.Channel.Id
+                                                               select t;
+
+            if(!x.Any())
+            {
+                DiscordEmbedBuilder error = new()
+                {
+                    Description = InsanityBot.LanguageConfig["insanitybot.tickets.add_user.not_a_ticket_channel"].ReplaceValues(
+                        ctx, ctx.Channel),
+                    Color = DiscordColor.Red
+                };
+
+                _ = ctx.Channel.SendMessageAsync(error.Build());
+                return;
+            }
+
+            foreach(var v in roles)
+            {
+                _ = ctx.Channel.AddOverwriteAsync(v, Permissions.AccessChannels);
+
+                if((Boolean)TicketDaemon.Configuration["insanitybot.tickets.ghost_mention_added_members"])
+                {
+                    DiscordMessage msg = await ctx.Channel.SendMessageAsync(v.Mention);
+                    _ = msg.DeleteAsync();
+                }
+            }
+        }
     }
 }
