ae57e54603109a4bfadbdc043c9335f9369cc812
ExaInsanity <nicolaiandorfer@outlook.com>
2021-08-17 13:10:23 +0200

    add enum json converters

---
+++ InsanityBot.MessageServices/Messages/Rules/RuleTargetJsonConverter.cs
@@ -0,0 +1,39 @@
+﻿using InsanityBot.MessageServices.Messages.Rules.Data;
+
+using Newtonsoft.Json;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsanityBot.MessageServices.Messages.Rules
+{
+    internal class RuleTargetJsonConverter : JsonConverter<RuleTarget>
+    {
+        public override RuleTarget ReadJson(JsonReader reader, Type objectType, RuleTarget existingValue, Boolean hasExistingValue, JsonSerializer serializer)
+        {
+            return ((String)reader.Value).ToLower() switch
+            {
+                "channel" => RuleTarget.Channel,
+                "member" => RuleTarget.Member,
+                "command" => RuleTarget.Command,
+                "prefix" => RuleTarget.Prefix,
+                _ => throw new InvalidDataException("Invalid JSON")
+            };
+        }
+        public override void WriteJson(JsonWriter writer, RuleTarget value, JsonSerializer serializer)
+        {
+            writer.WriteValue(value switch
+            {
+                RuleTarget.Channel => "channel",
+                RuleTarget.Member => "member",
+                RuleTarget.Command => "command",
+                RuleTarget.Prefix => "prefix",
+                _ => throw new InvalidProgramException("Non-existent enum field selected")
+            });
+        }
+    }
+}
