9c4e2d0a861aedecc7013d31d21b1ef0ebddd690
ExaInsanity <nicolaiandorfer@outlook.com>
2021-08-18 14:02:11 +0200

    add log event definitions

---
+++ InsanityBot.MessageServices/Messages/Rules/LoggerRule.cs
@@ -0,0 +1,33 @@
+﻿using InsanityBot.MessageServices.Messages.Rules.Data;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+using System;
+
+namespace InsanityBot.MessageServices.Messages.Rules
+{
+    public class LoggerRule
+    {
+        [JsonProperty("target")]
+        [JsonRequired]
+        [JsonConverter(typeof(RuleTargetJsonConverter))]
+        public RuleTarget Target { get; set; }
+
+        [JsonProperty("memberTarget")]
+        [JsonConverter(typeof(MemberRuleTargetJsonConverter))]
+        public MemberRuleTarget? MemberTarget { get; set; } = null;
+
+        [JsonProperty("channelTarget")]
+        [JsonConverter(typeof(ChannelRuleTargetJsonConverter))]
+        public ChannelRuleTarget? ChannelTarget { get; set; } = null;
+
+        [JsonProperty("parameter")]
+        [JsonRequired]
+        public JObject RuleParameter { get; set; }
+
+        [JsonProperty("allowAction")]
+        [JsonRequired]
+        public Boolean Allow { get; set; }
+    }
+}
